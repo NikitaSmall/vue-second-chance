02b444b3b33e953b82dea4e14e2d9de1a58f9798
NikitaSmall <nikitasosnov@yahoo.com>
2019-11-06 17:34:26 +0200

    cleared the project, prepare it for the start

---
+++ src/components/Root.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+  props: {
+    msg: String
+  }
+}
+</script>
